3a93ba5aaa048934b0baf420c4f8925abbefcd2e
ulyapuspadewi <41549424+ulyapuspadewi@users.noreply.github.com>
2022-03-14 09:17:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
+import React from 'react';
+import FormInput from './components/FormInput';
+import TodoItem from './components/TodoItem';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+   todos: [
+     {
+      id: 1,
+      title: "Bawang Merah 200 gr"
+     },
+     {
+      id: 2,
+      title: "Kol 1/2 kg"
+     }
+   ]
+  }
+
+  deleteTask = id => {
+    this.setState({
+      todos: this.state.todos.filter(item => item.id !== id)
+    })
+  }
+
+  render() {
+    const{ todos } = this.state;
+    return (
+      <div className="app">
+        <div className='logo'>
+          <img src={logo} alt="logo" />
+          <h3>Daftar Belanja</h3>
+          <div className='list'>
+            {todos.map(item =>
+              <TodoItem key={item.id} todo={item} del={this.deleteTask}/>
+              )}
+          </div>
+          <div className='input-form'>
+            <FormInput add={this.addTask}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+// class App extends React.Component {
+//   state = {
+//    todos: [
+//      {
+//       id: 1,
+//       title: "Bawang Merah 200 gr"
+//      },
+//      {
+//       id: 2,
+//       title: "Kol 1/2 kg"
+//      }
+//    ]
+//   }
+
+  // deleteTask = id => {
+  //   this.setState({
+  //     todos: this.state.todos.filter(item => item.id !== id)
+  //   })
+  // }
+
+  // addTask = data => {
+  //   const id = this.state.todos.length
+  //   const newData = {
+  //     id: id + 1,
+  //     title: data
+  //   }
+  //   this.setState({
+  //     todos: [...this.state.todos, newData]
+  //   })
+  // }
+  // addTask = data => {
+  //   const id = this.state.todos.length
+  //   const newData = {
+  //     id: id + 1,
+  //     title: data
+  //   }
+  //   this.setState({
+  //     todos: [...this.state.todos, newData]
+  //   })
+  // }
+
+  //   render() {
+  //     const{ todos } = this.state;
+  //     return (
+  //       <div className="app">
+  //         <div className='logo'>
+  //           <img src={logo} alt="logo" />
+  //           <h3>Daftar Belanja</h3>
+  //           <div className='list'>
+  //             {todos.map(item =>
+  //               <TodoItem key={item.id} todo={item} del={this.deleteTask}/>
+  //               )}
+  //           </div>
+  //           <div className='input-form'>
+  //             <FormInput add={this.addTask}/>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     );
+  //   }
+  // }
+
+// export default App;
